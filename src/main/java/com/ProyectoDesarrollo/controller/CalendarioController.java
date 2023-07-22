2eace0f09cbee44008f3d8aeeb3d949e3028e740
JoseLuis2510 <113859269+JoseLuis2510@users.noreply.github.com>
2023-07-22 12:25:27 -0600

    Calendario

---
+++ src/main/java/com/ProyectoDesarrollo/controller/CalendarioController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.ProyectoDesarrollo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/calendario")
+public class CalendarioController {
+    
+    @GetMapping("/calendario")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "/calendario/calendario";
+    }
+    
+}
