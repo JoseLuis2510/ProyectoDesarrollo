6890f5c67fa8714412b8640b60da2ddf3972851b
Andres Morera <114629703+andims17@users.noreply.github.com>
2023-08-19 21:56:40 -0600

    implementacion de reporte de tickets, funciona al 100%

--- src/main/java/com/ProyectoDesarrollo/controller/CrearTicket.java
+++ src/main/java/com/ProyectoDesarrollo/controller/CrearTicket.java
@@ -4,9 +4,13 @@
  */
 package com.ProyectoDesarrollo.controller;
 
+import com.ProyectoDesarrollo.domain.Ticket;
+import com.ProyectoDesarrollo.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,10 +21,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/creaTicket")
 public class CrearTicket {
     
+    @Autowired
+    TicketService ticketService;
+    
     @GetMapping("/creaTicket")
     public String page(Model model) {
         model.addAttribute("attribute", "value");
         return "creaTicket/creaTicket";
     }
+    
+    @PostMapping("/guardar")
+    public String ticketGuardar(Ticket ticket) {        
+        ticketService.save(ticket);
+        return "redirect:/ticket/listado";
+    }
+    
     
 }
--- src/main/java/com/ProyectoDesarrollo/domain/Ticket.java
+++ src/main/java/com/ProyectoDesarrollo/domain/Ticket.java
@@ -41,7 +41,4 @@ public class Ticket implements Serializable{
     public Ticket(){}
 
     
-
-    
-    
 }
